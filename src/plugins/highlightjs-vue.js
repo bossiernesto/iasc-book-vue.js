9ae02aab308f4e0e1fa836e3364f694769232526
Ernesto Bossi <bossi.ernestog@gmail.com>
2021-05-11 10:46:09 -0300

    removed unused components. Using minified versions of css

--- src/plugins/highlightjs-vue.js
+++ src/plugins/highlightjs-vue.js
@@ -1,7 +1,31 @@
 // Import Vue and vue-highlgihtjs
-import Vue from 'vue'
-import VueHighlightJS from 'vue-highlightjs'
-import 'highlight.js/styles/default.css' 
+import Vue from 'vue';
+import VueHighlightJS from 'vue-highlight.js';
+
+// Highlight.js languages (Only required languages)
+import cpp from 'highlight.js/lib/languages/cpp';
+import python from 'highlight.js/lib/languages/python';
+import elixir from 'highlight.js/lib/languages/elixir';
+import scala from 'highlight.js/lib/languages/scala';
+import haskell from 'highlight.js/lib/languages/haskell';
+import ruby from 'highlight.js/lib/languages/ruby';
+import javascript from 'highlight.js/lib/languages/javascript';
+import vue from 'vue-highlight.js/lib/languages/vue';
+
+
+import 'highlight.js/styles/github.css' 
  
 // Tell Vue.js to use vue-highlightjs
-Vue.use(VueHighlightJS)
+Vue.use(VueHighlightJS, {
+	// Register only languages that you want
+	languages: {
+    cpp,
+    elixir,
+    haskell,
+		javascript,
+    python,
+		ruby,
+    scala,
+		vue
+	}
+});
