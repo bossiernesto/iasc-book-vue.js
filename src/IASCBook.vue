4bc0ebaf42c9595635fe00d2865dfb85c90caa0c
Ernesto Bossi <bossi.ernestog@gmail.com>
2021-05-12 16:54:24 -0300

    Update

--- src/IASCBook.vue
+++ src/IASCBook.vue
@@ -11,10 +11,14 @@
           <AppLayout>
             <slot />
           </AppLayout>
-          <Sidebar         
-            :collapsed="collapsed"
-            :toggle-collapse="onToggleCollapse"
-          />
+          <sidebar-menu         
+            :menu="menu"
+            :width="sidebarWidth"
+            :collapsed="this.collapsed"
+            @toggle-collapse="onToggleCollapse"
+          >
+            <span slot="toggle-icon" class="fa fa-bars"></span>
+          </sidebar-menu>
       </div>
     </div>
     <Footer /> 
@@ -25,14 +29,14 @@
 import Footer from '@/components/Footer.vue'
 import AppLayout from '@/layouts/AppLayout'
 import { sidearContent } from '@/static/SidebarContent.js'
-import Sidebar from '@/components/Sidebar.vue'
+import { SidebarMenu } from 'vue-sidebar-menu'
 
 export default {
   name: 'IASCBook',
   components: {
     AppLayout,
     Footer,
-    Sidebar
+    SidebarMenu
   },
   data () {
     return {
@@ -90,5 +94,46 @@ export default {
   background-color: #000;
   opacity: 0.5;
   z-index: 900;
+}
+/* sidebar */
+.bm-menu {
+    background: #6B7A8F;
+    color: #fff;
+}
+
+.bm-menu .components {
+    padding: 20px 0;
+    border-bottom: 1px solid #6B7A8F;
+}
+
+.bm-menu ul p {
+    color: #fff;
+    padding: 10px;
+}
+
+.bm-menu ul li a {
+    padding: 10px;
+    font-size: 1.1em;
+    display: block;
+}
+.bm-menu ul li a:hover {
+    color: #7187a7;
+    background: #fff;
+}
+
+.bm-menu ul li.active > a, a[aria-expanded="true"] {
+    color: #fff;
+    background: #7187a7;
+}
+</style>
+<style lang="scss" scoped>
+.sidebar.v-sidebar-menu .vsm-arrow:after {
+  content: '\f105';
+  font-family: 'FontAwesome';
+}
+
+.sidebar.v-sidebar-menu .collapse-btn:after {
+  content: '\f07e';
+  font-family: 'FontAwesome';
 }
 </style>
